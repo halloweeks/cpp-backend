c4ff888513b7ad9bad4fb2863767e4dc1d1cda36
Hallo Weeks <96823158+halloweeks@users.noreply.github.com>
2022-02-04 10:47:41 +0530

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+if ! dpkg -s cmake >/dev/null 2>&1; then
+  echo -e "We not found cmake in your system\nPlease run apt-get install cmake"
+  exit 1;
+fi
+
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make
+cp server ../
+cp client ../
+rm -rf ../build
